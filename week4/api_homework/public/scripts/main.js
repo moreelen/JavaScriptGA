14826179b81b068be7c140fc44ecab588546644e
Rosa Carbo-Mascarell <r.carbomascarell@gmail.com>
2017-10-25 18:27:41 +0100

    testing colours on new server

--- week4/api_homework/public/scripts/main.js
+++ week4/api_homework/public/scripts/main.js
@@ -3,31 +3,33 @@ $(function(){
   var memoryTemplate = Handlebars.compile(source);
 
   var $loadButton = $("#load");
-  var $memoriesList = $("#memoriesList")
+  var $memoriesList = $("#memoriesList");
   var $addForm = $("#addForm");
   var $author = $("#author");
   var $age = $("#age");
   var $location = $("#location");
   var $memory = $("#memory");
-
-  console.log('Buuuummmm');
+  var colours = ['#556270', '#4ECDC4', '#C7F464', '#FF6B6B', '#C44D58'];
+  var $colourMemories = $("#memories li");
+  // console.log('$memoriesList', $memoriesList);
+  // console.log('$colourMemories', $colourMemories);
 
   function errorHandler(error){
     console.error(error);
   }
 
   function getMemories(){
-    console.log('getMemories');
+    // console.log('getMemories');
     $.ajax({
       url: '/memories'
     }).done(function(response){
-      console.log(response);
+      // console.log(response);
       writeMemories(response);
     }).fail(errorHandler());
   }
 
   function writeMemories(memories){
-    console.log('writeMemories');
+    // console.log('writeMemories');
     $memoriesList.html('');
     memories.forEach(function(memory){
       $memoriesList.append(memoryTemplate(memory));
@@ -35,7 +37,7 @@ $(function(){
   }
 
   function addMemory(memory){
-    console.log('Adding memory.');
+    // console.log('Adding memory.');
     $.ajax({
       method: 'POST',
       url: '/memory',
@@ -46,7 +48,7 @@ $(function(){
   }
 
   function deleteMemory(id){
-    console.log('Delete ' + id);
+    // console.log('Delete ' + id);
     $.ajax({
       method: 'DELETE',
       url: '/memory/' + id
@@ -55,8 +57,25 @@ $(function(){
     }).fail(errorHandler);
   }
 
+  function randomColour(){
+    var colourPicker = Math.floor(Math.random() * 5);
+    return colourPicker;
+  }
+
+  function colourMemory(){
+    console.log('colour:', colours[randomColour()]);
+    //$colourMemories.forEach(function(memory){
+    //  console.log(memory);
+    //});
+    //.css({
+    //  "background-color" : colours[randomColour()]
+    //});
+  }
+
+  colourMemory();
+
   $loadButton.on('click.load', function(){
-    console.log('clicked');
+    // console.log('clicked');
     getMemories();
   });
 
@@ -67,7 +86,7 @@ $(function(){
       location: $location.val(),
       memory: $memory.val()
     }
-    console.log('newMemory', newMemory);
+    // console.log('newMemory', newMemory);
     addMemory(newMemory);
     $author.val('');
     $age.val('');
@@ -79,7 +98,7 @@ $(function(){
   $($memoriesList).on('click.delete', '.delete-button', function(){
     var $button = $(this);
     var memoryDeletedId = $button.data('id');
-    console.log('Deleting ', memoryDeletedId);
+    // console.log('Deleting ', memoryDeletedId);
     deleteMemory(memoryDeletedId);
     $button.parents('li').remove();
   });
